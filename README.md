8d10a0c9b517f7d60064f860f47dfff8ab03ae6e
William Lillis <wjlillis22@wabash.edu>
2023-04-12 18:38:24 -0400

    - Finished README.md for graders. - Final changes before submission

--- README.md
+++ README.md
@@ -13,4 +13,6 @@ The code's dependencies are:
 - the ``random`` module
 
 Each run of the program as it is currently configured completes a k-folds cross validation test of both the wine and congressional datasets, with ``k=5`` folds. 
-The number of folds and number of trees in the random forest can be configured via the arguments passed to ``
+The number of folds and number of trees in the random forest can be configured via the arguments passed to ``test_wine()`` and ``test_congress()`` inside of ``src.py``.
+
+The splitting and stopping criteria can be configured by altering the corresponding arguments passed the ``decision_tree`` constructor within the ``random_forest`` constructor.
--- src.py
+++ src.py
@@ -4,65 +4,6 @@ from decision_tree import prepare_data
 from misc import bootstrap
 from misc import k_folds_gen
 
-# general testing purposes
-def main():
-    k_folds, attr_type, data_labels = k_folds_gen(10, 'hw3_house_votes_84.csv')
-    print('hw3_house_votes_84.csv')
-    for fold in k_folds:
-        #print(f"New fold: {fold}\n\n")
-        pass
-
-    data = k_folds[1]
-    for i in range(2, len(k_folds)):
-        data += k_folds[i]
-    
-    # slap the labels back onto the top of the k_folds list of lists
-    data.insert(0, data_labels)
-    #print(data)
-    #k_folds, attr_type, data_labels = k_folds_gen(10, 'hw3_wine.csv')
-    
-    #print('hw3_wine.csv')
-    #for fold in k_folds:
-    #    print(f"New fold: Size: {len(fold)}\n\n")
-    #    pass
-
-    #data = k_folds[1]
-    #for i in range(2, len(k_folds)):
-    #    data += k_folds[i]
-    
-    # slap the labels back onto the top of the k_folds list of lists
-    # ONLY FOR WINE?
-    #data.insert(0, data_labels)
-    data_labels_num = deepcopy(data_labels)
-    # vvvvv for wine vvvvv
-    for i in range(len(data_labels_num) - 1):
-        #data_labels_num[i] = int(data_labels_num[i])
-        data_labels_num[i] = i
-    # vvvvv for congress vvvvv
-    #for i in range(len(data_labels_num) - 1):
-    #    data_labels_num[i] = i
-
-    #print(data)
-    #BUGBUG check how data_labels behaves with the different data sets
-    forest = random_forest.random_forest(data, 10, attr_type, data_labels_num)
-    forest.recur_print()
-
-    num_correct = 0
-    num = 0
-    for entry in k_folds[0]:
-        #print(f"class: {entry[-1]}")
-        num += 1
-        #BUGBUG need to pass attr_to_index here
-        if forest.classify_instance(entry, attr_type) == entry[-1]:
-            #print("Great success!")
-            num_correct += 1
-        else:
-            #print("Failure!")
-            pass
-    print(f"Score: {num_correct / num}")
-
-    print(f"attr_type: {attr_type}")
-
 # wine dataset
 def test_wine(num_trees: int, num_folds: int):
     k_folds, attr_type, data_labels = k_folds_gen(num_folds, 'hw3_wine.csv')
@@ -232,6 +173,5 @@ def test_congress(num_trees: int, num_folds: int):
     print(f"\tAvg F1 Score: {sum(F1s) / len(F1s)}")
 
 if __name__ == "__main__":
-    #main()
     test_wine(50, 5)
     test_congress(50, 5)
